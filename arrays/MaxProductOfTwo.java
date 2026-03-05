354a1c0a960bef4aadfb73fbe90fc05cd81ea1b5
Rohith <rohith18raj@gmail.com>
2026-03-05 18:57:24 +0530

    Maximum product of two elements in an array

---
+++ arrays/MaxProductOfTwo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class MaxProductOfTwo{
+	 public static int maxProduct(int[] nums) {
+        int max1 = 0;
+        int max2 = 0;
+        for(int i:nums){
+            if(i > max1){
+                max2 = max1;
+                max1 = i;
+            }else if(i > max2){
+                max2 = i;
+            }
+        }
+        return (max1 - 1)*(max2 - 1);
+    }
+    public static void main(String[] args){
+    	int[] arr = {3,4,5,2};
+    	System.out.println(maxProduct(arr));
+    }
+}
