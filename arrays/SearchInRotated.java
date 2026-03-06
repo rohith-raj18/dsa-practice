df36b5fc60c6b500976e00a3240366976ee0ab37
Rohith <rohith18raj@gmail.com>
2026-03-06 19:10:09 +0530

    search in rotated sorted Array

---
+++ arrays/SearchInRotated.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class SearchInRotated {
+    public static int search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length-1;
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[i] <= nums[mid]){
+                if(target >= nums[i] && target < nums[mid]){
+                    j = mid -1;
+                }else{
+                    i = mid + 1;
+                }
+            }else{
+                if(target > nums[mid] && target <= nums[j]){
+                    i = mid + 1;
+                }else{
+                    j = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(search(arr,1));
+     }
+}
