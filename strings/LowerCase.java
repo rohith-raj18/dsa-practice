e4caf85fb934c0db8f018fee78e7ab2b5a5dc5f1
Rohith <rohith18raj@gmail.com>
2026-03-12 17:51:20 +0530

    to lower case

---
+++ strings/LowerCase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class LowerCase{
+	public static String toLowerCase(String s){
+		return s.toLowerCase();
+	}
+	public static void main(String[] args){
+		System.out.println(toLowerCase("HELLO"));
+	}
+}
