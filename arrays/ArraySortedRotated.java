0983fc182f0d5204e46d5fdb8ae286c21ee2f57c
Rohith <rohith18raj@gmail.com>
2026-03-24 17:53:29 +0530

    check if array is sorted and rotated

---
+++ arrays/ArraySortedRotated.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ArraySortedRotated {
+    public static boolean check(int[] nums) {
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] > nums[(i+1)%nums.length]){
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+    public static void main(String[] args){
+       int[] arr = {3,4,5,1,2};
+        System.out.println(check(arr));
+     }
+}
