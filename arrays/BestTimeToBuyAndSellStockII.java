a5cb8b04a52bdf802b16a1f418855a343179aaa8
Rohith <rohith18raj@gmail.com>
2026-04-07 18:02:51 +0530

    Best time to buy and sell stock II

---
+++ arrays/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class BestTimeToBuyAndSellStockII{
+	public static int maxProfit(int[] prices) {
+        
+        int max = 0;
+        for(int i = 1;i<prices.length;i++){
+            if(prices[i] > prices[i-1]){
+                max += prices[i] - prices[i-1];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args){
+    	int[] arr = {7,1,5,3,6,4};
+    	System.out.println(maxProfit(arr));
+    }
+}
